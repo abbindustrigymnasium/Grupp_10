0303b2a5efcfafe9d46938f3e76e358e174b44cf
ABBguseve <43981730+ABBguseve@users.noreply.github.com>
2018-12-11 14:52:01 +0100

    Update light2.js

--- Backend/api/routes/light2.js
+++ Backend/api/routes/light2.js
@@ -59,7 +59,8 @@ router.get('/', (req, res, next) => {
 });
 
 
-router.post('/', (req, res) => { //  '/'= indexfilen i localhost/products/   req = request , res= resultat , next = det som ska hända senare 
+router.post('/', (req, res) => { 
+    //  '/'= indexfilen i localhost/products/   req = request , res= resultat , next = det som ska hända senare 
     Lights = [req.body.name, req.body.hard, req.body.strength];
     console.log(req.body); //Här skriver jag ut req.body för att se skillnaden på den mellan en postman request och en html.
     var createProduct = function () { //Skapar funktionen
@@ -78,7 +79,8 @@ router.post('/', (req, res) => { //  '/'= indexfilen i localhost/products/   req
         res.status(200).json({ //Här kan man som jag gjort lägga till en status 200, OBS tänk på att 201 kommer
             message: "Success, new light logged" //I detta exempel skickar 200 bara produkten medans 201 skickar meddelandet med.
         });
-        //Tog bort res.status(201) då dubbla status skickningar skapar error, vill man ha möjlighet till bägge så gör if else satser och lägg dem i.
+//Tog bort res.status(201) då dubbla status skickningar skapar error,
+ //vill man ha möjlighet till bägge så gör if else satser och lägg dem i.
     }).catch(err => {
         console.log(err);
         res.status(500).json({
@@ -88,12 +90,14 @@ router.post('/', (req, res) => { //  '/'= indexfilen i localhost/products/   req
 });
 
 
-router.patch('/', (req, res) => { //  '/'= indexfilen i localhost/products/   req = request , res= resultat , next = det som ska hända senare 
+router.patch('/', (req, res) => { 
+    //  '/'= indexfilen i localhost/products/   req = request , res= resultat , next = det som ska hända senare 
     Lights = [req.body.hard, req.body.strength, req.body.name];
     console.log(req.body); //Här skriver jag ut req.body för att se skillnaden på den mellan en postman request och en html.
     var createProduct = function () { //Skapar funktionen
         return new Promise(function (resolve, reject) { //Skapar löftet
-            con.query('UPDATE light SET `hard` = ? , `strength` = ? Where `name` = ?', [Lights[0], Lights[1], Lights[2]], function (err, result) {
+            con.query('UPDATE light SET `hard` = ? , `strength` = ? Where `name` = ?',
+                      [Lights[0], Lights[1], Lights[2]], function (err, result) {
                 if (err) {
                     return reject(err);
                 } else {
